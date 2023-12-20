22eb2d8efa2ef5bad0d5538ccb6752bb7b6ca539
July Sudarenko <july@sudarenko.ru>
2023-12-20 22:05:24 +0300

    First commit

---
+++ hw1/hello.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+int main(int argc, char **argv)
+{
+    setlocale(LC_ALL,"Rus");
+/*
+(\___/)
+(='.'=)
+(")_(")
+*/
+
+    printf("\t(\\___/)\n");
+    printf("\t(='.'=)\n");
+    printf("\t(\")_(\")\n");
+
+
+
+    return 0;
+}
+
